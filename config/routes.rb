86740b4887f3b1a146da8b5962849512fea82ac6
Andrea del Rio <adelrio@gmail.com>
2017-03-14 15:59:39 -0700

    add static_pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/root'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#root"
+  
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
+  end
+
 end
