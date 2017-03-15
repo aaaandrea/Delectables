e50f717ede9772d4713e10609910419d9a79af92
Andrea del Rio <adelrio@gmail.com>
2017-03-14 18:00:02 -0700

    app/root fix, add session form and user

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import UserContainer from './user/user_container';
 
 const App = ({ children }) => (
   <div>
     <h1>Delectables</h1>
+    <UserContainer />
     { children }
   </div>
 );
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {  Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './app';
 import { Provider } from 'react-redux';
+import SessionFormContainer from './session_form/session_form_container';
 
 const Root = ({ store }) => {
   debugger;
@@ -9,6 +10,8 @@ const Root = ({ store }) => {
   <Provider store={ store }>
     <Router history={ hashHistory }>
       <Route path="/" component={ App } />
+        <Route path="/login" component={ SessionFormContainer } />
+        <Route path="/signup" component={ SessionFormContainer } />
     </Router>
   </Provider>
 );
