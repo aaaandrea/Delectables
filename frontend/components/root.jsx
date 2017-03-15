635917838962176c84c5ad2aa1048d870f3dc6fc
Andrea del Rio <adelrio@gmail.com>
2017-03-14 17:09:19 -0700

    add static pages, correct root

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,10 +1,17 @@
-import {  Router, Route, IndexRoute, hashHistory } from 'react-redux';
+import React from 'react';
+import {  Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './app';
+import { Provider } from 'react-redux';
 
-const Root = ({ store }) => (
+const Root = ({ store }) => {
+  debugger;
+  return(
   <Provider store={ store }>
     <Router history={ hashHistory }>
       <Route path="/" component={ App } />
     </Router>
   </Provider>
 );
+};
+
+export default Root;
