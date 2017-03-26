c178f014cc7e3a95707c7763600cd9cd8ac9ee04
Andrea del Rio <adelrio@gmail.com>
2017-03-25 17:27:30 -0700

    add https to background images

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -12,7 +12,7 @@ class Home extends React.Component {
     return(
       <div className="home-container">
         <img className="home-img"
-          src="http://res.cloudinary.com/dg0cmca7a/image/upload/v1489704383/michael-browning-188999_agnvtl.jpg"
+          src="https://res.cloudinary.com/dg0cmca7a/image/upload/v1489704383/michael-browning-188999_agnvtl.jpg"
           alt="glasses" />
         <RecipeIndex/>
       </div>
