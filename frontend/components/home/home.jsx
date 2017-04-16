f0631466bac547a7c567ed027103c821bb10238b
Andrea del Rio <adelrio@gmail.com>
2017-04-16 13:19:22 -0700

    color scheme change

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -11,9 +11,11 @@ class Home extends React.Component {
   render(){
     return(
       <div className="home-container">
-        <img className="home-img"
-          src="https://res.cloudinary.com/dg0cmca7a/image/upload/v1489704383/michael-browning-188999_agnvtl.jpg"
-          alt="glasses" />
+        <div className="img-container">
+          <img className="home-img"
+            src="https://res.cloudinary.com/dg0cmca7a/image/upload/v1489704383/michael-browning-188999_agnvtl.jpg"
+            alt="glasses" />
+        </div>
         <RecipeIndex/>
       </div>
     );
